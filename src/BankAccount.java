cb9dc4056d964cd724d13032a026763ae136d91f
fendi <theobaldus24@jiu.com>
2026-03-30 19:58:27 +0700

    Add deposit and withdraw methods

---
+++ src/BankAccount.java
@@ -0,0 +1,52 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class BankAccount {
+    private double balance;
+    private List<Transaction> transactions;
+
+    public BankAccount() {
+        this.balance = 0;
+        this.transactions = new ArrayList<>();
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public List<Transaction> getTransactions() {
+        return transactions;
+    }
+
+    public void addBalance(double amount) {
+        this.balance += amount;
+    }
+
+    public void reduceBalance(double amount) {
+        this.balance -= amount;
+    }
+
+    public void addTransaction(Transaction transaction) {
+        transactions.add(transaction);
+    }
+    public void deposit(double amount) throws InvalidAmountException {
+    if (amount <= 0) {
+        throw new InvalidAmountException("Deposit amount must be greater than 0.");
+    }
+
+    addBalance(amount);
+    addTransaction(new Transaction(TransactionType.DEPOSIT, amount, balance));
+}
+
+public void withdraw(double amount) throws InvalidAmountException, InsufficientBalanceException {
+    if (amount <= 0) {
+        throw new InvalidAmountException("Withdraw amount must be greater than 0.");
+    }
+
+    if (amount > balance) {
+        throw new InsufficientBalanceException("Withdraw cannot exceed balance.");
+    }
+
+    reduceBalance(amount);
+    addTransaction(new Transaction(TransactionType.WITHDRAW, amount, balance));
+}
