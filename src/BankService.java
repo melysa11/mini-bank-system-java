b9b469c93229da6d22775b5717fa964995cab343
umbu24-max <umbu24@jiu.ac>
2026-03-30 22:17:45 +0700

    Update BankService.java

--- src/BankService.java
+++ src/BankService.java
@@ -1,29 +1,16 @@
 public class BankService {
+  private BankAccount account;
 
-private BankAccount
-
-account;
-
-public BankService() { account = new
-
-BankAccount();
-
+public BankService() {
+  this.account = new BankAccount();
 }
 
 public void deposit(double amount) throws InvalidAmountException {
+  account.deposit(amount);
+}
+  
+public void withdraw(double amount) throws InvalidAmountException, InsufficientBalanceException {
+        account.withdraw(amount);
+  }
+}
 
-account.deposit(amount);
-
-public void
-
-withdraw(double amount)
-
-throws
-
-ceException
-
-InvalidAmountException,
-
-InsufficientE
-
-{
