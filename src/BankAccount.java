27d2cf5918aafd57ca77f0b045a339ea54febd3c
fendisobang <theobaldus24@jiu.ac>
2026-03-31 09:13:36 +0700

    Update BankAccount.java

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -2,12 +2,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class BankAccount {
+    private String name;
     private double balance;
     private List<Transaction> transactions;
 
-    public BankAccount() {
-        this.balance = 0;
+    public BankAccount(String name, double balance) {
+        this.name = name;
+        this.balance = balance;
         this.transactions = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
     }
 
     public double getBalance() {
@@ -18,36 +24,22 @@ public class BankAccount {
         return transactions;
     }
 
-    public void addBalance(double amount) {
-        this.balance += amount;
-    }
-
-    public void reduceBalance(double amount) {
-        this.balance -= amount;
-    }
-
-    public void addTransaction(Transaction transaction) {
-        transactions.add(transaction);
-    }
     public void deposit(double amount) throws InvalidAmountException {
-    if (amount <= 0) {
-        throw new InvalidAmountException("Deposit amount must be greater than 0.");
+        if (amount <= 0) {
+            throw new InvalidAmountException("Deposit must be > 0");
+        }
+        balance += amount;
+        transactions.add(new Transaction(TransactionType.DEPOSIT, amount, balance));
     }
 
-    addBalance(amount);
-    addTransaction(new Transaction(TransactionType.DEPOSIT, amount, balance));
-}
-
-public void withdraw(double amount) throws InvalidAmountException, InsufficientBalanceException {
-    if (amount <= 0) {
-        throw new InvalidAmountException("Withdraw amount must be greater than 0.");
+    public void withdraw(double amount) throws InvalidAmountException, InsufficientBalanceException {
+        if (amount <= 0) {
+            throw new InvalidAmountException("Withdraw must be > 0");
+        }
+        if (amount > balance) {
+            throw new InsufficientBalanceException("Not enough balance");
+        }
+        balance -= amount;
+        transactions.add(new Transaction(TransactionType.WITHDRAW, amount, balance));
     }
-
-    if (amount > balance) {
-        throw new InsufficientBalanceException("Withdraw cannot exceed balance.");
-    }
-
-    reduceBalance(amount);
-    addTransaction(new Transaction(TransactionType.WITHDRAW, amount, balance));
-}
 }
